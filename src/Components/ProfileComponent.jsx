7aa57d5456b876f56159c8a58e0493d9c6abd765
TasmiaH0508 <tasmiahaque2004@gmail.com>
2024-05-30 17:39:55 +0800

    Update ProfileComponent.jsx

--- src/Components/ProfileComponent.jsx
+++ src/Components/ProfileComponent.jsx
@@ -1,13 +1,54 @@
 import logo from "../assets/logo.svg"
+import browserLG from "../assets/browserLG.svg"
 import '../Sass/ProfileComponent.scss'
+import React, { useState } from 'react';
+import InputProcessor from "../Helpers/InputProcesser.jsx"
+import createComponentWhenClicked from "../Helpers/createComponentWhenClicked.jsx";
+
 const ProfileComponent = () => {
+
+    const input1 = InputProcessor("")
+    const str1 = "Tell us your interests:"
+    const comp1 = <div>
+        <div>*This information will be displayed.</div>
+        <div>*Omit hyphen for hyphenated words.</div>
+    </div>
+    const str2= "Module code of module you want to tutor:"
+    const comp2 = <div>
+        <div>E.g. PH1251</div>
+    </div>
     return (<div>
-        <img src={logo} width={300}/>
-        <div>Name(as in student ID):
+        <img src={logo} alt="logo" width={300}/>
+        <div>Let us get to know you better!</div>
+        <div>Name(as in student ID):</div>
+        <input/>
+        <div>I am joining NUSConnect to make new friends:</div>
+        <ul className="ul">
+            <li className="li">No</li>
+            {createComponentWhenClicked(input1, str1, comp1)}
+        </ul>
+        <div>I am joining NUSConnect to find a tutor:</div>
+        <ul className="ul">
+            <li className="li">No</li>
+            <li className="li">Yes</li>
+        </ul>
+        <div>I am joining NUSConnect to as a volunteer</div>
+        <div>tutor:</div>
+        <div>
+            <ul className="ul">
+                <li className="li">No</li>
+                {createComponentWhenClicked(input1, str2, comp2)}
+            </ul>
         </div>
         <div>
-            <div>Tell us why you are joining NUSConnect:</div>
+            <button className="button">
+                <div>Connect to NUSConnect</div>
+                <img src={browserLG}
+                     alt="Connect Now"
+                     width={50}/>
+            </button>
         </div>
     </div>)
 }
+
 export default ProfileComponent;
