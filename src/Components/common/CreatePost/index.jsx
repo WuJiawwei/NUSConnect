3434965db5ac9b62be9921cad07de6b237018ce7
Jiawei <e1155577@u.nus.edu>
2024-05-31 22:00:31 +0800

    Create Post Front-end

---
+++ src/Components/common/CreatePost/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import "./index.scss"
+import ModalComponent from "../Modal"
+
+export default function PostStatus() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [status, setStatus] = useState("");
+  return (
+    <div className="post-status-main">
+        <div className="post-status">
+            <button className="open-post-modal" onClick={() => setModalOpen(true)}>
+                Start a Post
+            </button>
+        </div>
+
+        <ModalComponent 
+          setStatus={setStatus}
+          modalOpen={modalOpen} 
+          setModalOpen={setModalOpen}
+          status={status} 
+        />
+    </div>
+  )
+}
--- src/Components/common/Topbar/index.jsx
+++ src/Components/common/Topbar/index.jsx
@@ -5,15 +5,28 @@ import { FaHome, FaUsers, FaBriefcase, FaSearch} from "react-icons/fa"
 import { MdMessage} from "react-icons/md"
 import { BiSolidBellRing} from "react-icons/bi"
 import user from "../../../assets/user.png"
+import { useNavigate } from 'react-router-dom'
 
 export default function Topbar() {
+  let navigate = useNavigate()
+  const goToRoute = (route) => {
+    navigate(route)
+  }
   return (
     <div className="topbar-main">
         <img className="project-Logo" src={projectLogo} alt="projectLogo" />
         <div className="react-icons">
           <FaSearch size={30} className="react-icon"/>
-          <FaHome size={30} className="react-icon"/>
-          <FaUsers size={30} className="react-icon"/>
+          <FaHome 
+            size={30} 
+            className="react-icon"
+            onClick={() => goToRoute("/home")}
+          />
+          <FaUsers 
+            size={30} 
+            className="react-icon"
+            onClick={() => goToRoute("/profile")}
+            />
           <FaBriefcase size={30} className="react-icon"/>
           <MdMessage size={30} className="react-icon" />
           <BiSolidBellRing size={30} className="react-icon" />
