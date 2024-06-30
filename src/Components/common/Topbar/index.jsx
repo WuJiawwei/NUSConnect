fe061ef1e9fd630bd84a8df49fc898fbda5439ee
TasmiaH0508 <tasmiahaque2004@gmail.com>
2024-06-30 13:46:25 +0800

    Update index.jsx

--- src/Components/common/Topbar/index.jsx
+++ src/Components/common/Topbar/index.jsx
@@ -1,44 +1,56 @@
 import React from 'react'
+import {useState} from "react";
 import "./index.scss"
 import projectLogo from "../../../assets/projectLogo.png"
-import { FaHome, FaUsers, FaBriefcase, FaSearch} from "react-icons/fa"
+import { FaHome, FaUsers, FaBriefcase, FaSearch, FaPowerOff} from "react-icons/fa"
 import { MdMessage} from "react-icons/md"
 import { BiSolidBellRing} from "react-icons/bi"
 import { RiTimerFill } from 'react-icons/ri'
 import { RiProfileFill } from 'react-icons/ri'
 import user from "../../../assets/user.png"
 import { useNavigate } from 'react-router-dom'
+import PowerOff from "../Popups/PowerOff.jsx"
 
 export default function Topbar() {
   let navigate = useNavigate()
+    const [isOn, setIsOn] = useState(false)
   const goToRoute = (route) => {
     navigate(route)
   }
   return (
-    <div className="topbar-main">
-        <img className="project-Logo" src={projectLogo} alt="projectLogo" />
+      <div className="topbar-main">
+        <img className="project-Logo" src={projectLogo} alt="projectLogo"/>
         <div className="react-icons">
-          <FaSearch 
-            size={30} 
-            className="react-icon"
-            onClick={() => goToRoute("/redirectsearch")}
+          <FaSearch
+              size={30}
+              className="react-icon"
+              onClick={() => goToRoute("/redirectsearch")}
           />
-          <FaHome 
-            size={30} 
-            className="react-icon"
-            onClick={() => goToRoute("/home")}
+          <FaHome
+              size={30}
+              className="react-icon"
+              onClick={() => goToRoute("/home")}
           />
-          <RiProfileFill 
-            size={30} 
-            className="react-icon"
-            onClick={() => goToRoute("/profile")}
-            />
+          <RiProfileFill
+              size={30}
+              className="react-icon"
+              onClick={() => goToRoute("/profile")}
+          />
           <FaUsers size={30} className="react-icon"/>
           <RiTimerFill size={30} className="react-icon"/>
-          <MdMessage size={30} className="react-icon" />
-          <BiSolidBellRing size={30} className="react-icon" />
+          <MdMessage size={30} className="react-icon"/>
+          <BiSolidBellRing size={30} className="react-icon"/>
+          <FaPowerOff
+              size={30}
+              className="react-icon"
+              onClick = {() => setIsOn(!isOn)}
+          />
         </div>
-        <img className="user-logo" src={user} alt="user" />
-    </div>
+
+          {isOn ? <PowerOff onClose={() => setIsOn(!isOn)}/> : <div></div>}
+
+        <img className="user-logo" src={user} alt="user"/>
+
+      </div>
   )
 }
