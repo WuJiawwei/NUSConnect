eb500043f6e2bbe07d6f6463dd93edd9ab03ef75
TasmiaH0508 <tasmiahaque2004@gmail.com>
2024-07-12 21:59:00 +0800

    Update Index.jsx

--- src/Routes/Index.jsx
+++ src/Routes/Index.jsx
@@ -2,14 +2,12 @@ import {createBrowserRouter} from "react-router-dom";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
 import ProfileLayout from "../layouts/ProfileLayout.jsx";
-import HomeLayout from "../layouts/HomeLayout.jsx"
-import RedirectSearch from "../Pages/RedirectSearch.jsx"
 import SearchTutor from "../Pages/SearchTutor.jsx";
 import SearchFriend from "../Pages/SearchFriend.jsx";
 import Welcome from "../Pages/Welcome.jsx";
 import CreateProfile from "../Components/common/EditProfile/createProfile.jsx";
-import DisplayAllChats from "../Components/common/DisplayAllChats/DisplayAllChats.jsx";
 import Chat from "../Components/common/Chat/chat.jsx"
+import Topbar from "../Components/common/Topbar/index.jsx"
 
 export const router = createBrowserRouter([
     {
@@ -22,15 +20,11 @@ export const router = createBrowserRouter([
     },
     {
       path:"/home",
-      element: <HomeLayout />
+      element: <Topbar/>
     },
     {
         path:"/profile",
         element: <ProfileLayout />
-    },
-    {
-        path:"/redirectsearch",
-        element: <RedirectSearch />
     },
     {
         path:"/searchtutor",
@@ -47,10 +41,6 @@ export const router = createBrowserRouter([
     {
         path:"/createprofile",
         element: <CreateProfile />
-    },
-    {
-        path:"/allchats",
-        element: <DisplayAllChats />
     },
     {
         path: "/chat",
