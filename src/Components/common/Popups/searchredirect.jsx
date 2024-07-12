560d35d1529603c48275f3c3e7a498816fe285ba
TasmiaH0508 <tasmiahaque2004@gmail.com>
2024-07-12 21:55:40 +0800

    Update searchredirect.jsx

--- src/Components/common/Popups/searchredirect.jsx
+++ src/Components/common/Popups/searchredirect.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import './index.scss';
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 
-const SearchPopup = () => {
+const SearchPopup = ({func, currUser}) => {
 
+    const [isFriendSearch, setIsFriendSearch] = useState(false);
     let navigate = useNavigate();
+
     return (
-        <div className="overlay">
-            <div className="popup">
-                <button className="close-button" onClick={() => navigate("/home")}>X</button>
-                <div>Who are you looking for?</div>
-                <div className="search-container">
-                    <button className="select-choice-button" onClick={() => navigate("/searchfriend")}>
-                        A friend
-                    </button>
-                    <button className="select-choice-button" onClick={() => navigate("/searchtutor")}>
-                        A tutor
-                    </button>
+        <div>
+            <div className="overlay">
+                <div className="popup">
+                    <button className="close-button" onClick={func}>X</button>
+                    <div>Who are you looking for?</div>
+                    <div className="search-container">
+                        <button
+                            className="select-choice-button"
+                            onClick={() => navigate("/searchfriend")}
+                        >
+                            A friend
+                        </button>
+                        <button
+                            className="select-choice-button"
+                            onClick={() => navigate("/searchtutor")}
+                        >
+                            A tutor
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     );
+
 };
 
 export default SearchPopup;
