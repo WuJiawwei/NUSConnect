28a1bc6e68d73ff6c9f99487ccbbf17460411224
TasmiaH0508 <tasmiahaque2004@gmail.com>
2024-07-12 21:56:31 +0800

    Update FriendSearch.jsx

--- src/Components/common/Search/FriendSearch.jsx
+++ src/Components/common/Search/FriendSearch.jsx
@@ -7,6 +7,7 @@ import { getCurrentUser } from "../../../api/FirestoreAPI.jsx";
 import FriendProfileModal from "../Popups/FriendProfile.jsx";
 
 const FriendSearch = () => {
+    // todo: try to implement this using props
     const [search, setSearch] = useState('');
     const [doc, setDoc] = useState([]);
     const [currentUser, setCurrentUser] = useState(null);
