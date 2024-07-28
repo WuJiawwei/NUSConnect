9d42ffbd4667afa3ae4133e6db40657813340c5d
TasmiaH0508 <tasmiahaque2004@gmail.com>
2024-07-28 14:39:32 +0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB_5eDOHgD_1ynCo78kwyVvYRFqxNSi85c",
-    authDomain: "databasetest-7900a.firebaseapp.com",
-    projectId: "databasetest-7900a",
-    storageBucket: "databasetest-7900a.appspot.com",
-    messagingSenderId: "784939490292",
-    appId: "1:784939490292:web:97c8a52645e87d21a077a1",
-    measurementId: "G-JNCVJ99NBF"
+    // my firebase configuration
 };
 
 // Initialize Firebase
